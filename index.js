80b57cfccf7b9adbb0ed5085bd338852ab6e467a
Jasmin Atay <yasematayy@gmail.com>
2021-02-19 21:00:50 +0100

    To-Do-App created

---
+++ index.js
@@ -0,0 +1,29 @@
+function Add(){
+    let input = document.getElementById('myInput');
+    let itemList = document.getElementById('myUL');
+    if(input.value===""){
+        alert('Please enter some text');
+    }else{
+        let item = document.createElement('li');
+        item.className = 'item';
+        item.innerHTML= input.value;
+        
+        let closeBtn = document.createElement('a');
+        closeBtn.setAttribute('href','#');
+        closeBtn.className='close';
+        closeBtn.innerHTML = "X";
+        
+        item.appendChild(closeBtn);
+        input.value="";
+        itemList.appendChild(item);
+        item.addEventListener('click', deleteBtn);
+    }
+    
+}
+
+function deleteBtn(e){
+    this.classList.add('lineThrough');
+    if(e.target.innerHTML==='X'){
+        this.parentNode.removeChild(this)
+    }
+}
